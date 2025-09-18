82e11cf053b18d7075fc40c1278695db682f6194
Roman Khadraliev <Romch33tos@yandex.ru>
2025-09-18 08:38:28 +0700

    Реализован класс для обработки событий

---
+++ MainPresenter.cs
@@ -0,0 +1,33 @@
+﻿namespace NumericalMethodsApp
+{
+  public class MainPresenter
+  {
+    private readonly IMainView _view;
+
+    public MainPresenter(IMainView view)
+    {
+      _view = view;
+    }
+
+    public void OpenMethod(string methodName)
+    {
+      string message = $"Открытие метода: {methodName}\n\n";
+      message += "Эта функциональность будет реализована в будущем.";
+
+      _view.ShowMessage(message);
+    }
+
+    public void ShowAbout()
+    {
+      string aboutText = "Численные методы v1.0\n\n" +
+                        "Приложение для реализации основных численных методов:";
+
+      _view.ShowMessage(aboutText);
+    }
+
+    public void ExitApplication()
+    {
+      _view.CloseApplication();
+    }
+  }
+}
