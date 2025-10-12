147b4d08e9e7f48210281d57da39c29f4062e8b8
Roman Khadraliev <Romch33tos@yandex.ru>
2025-10-12 21:17:27 +0700

    Добавлен файл с логикой виджетов окна

---
+++ GoldenRatioMethod/GoldenRatioMethod.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NumericalMethodsApp
+{
+  public partial class GoldenRatioView : Window
+  {
+    public GoldenRatioView()
+    {
+      InitializeComponent();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+    {
+
+    }
+  }
+}
