f7f37db4ca93a29032fbca291c242583588fd010
Roman Khadraliev <Romch33tos@yandex.ru>
2025-10-12 21:20:56 +0700

    Реализовано открытие окна метода золотого сечения

--- MainPresenter.cs
+++ MainPresenter.cs
@@ -1,3 +1,6 @@
+using System.Windows;
+using System.Windows.Controls;
+
 namespace NumericalMethodsApp
 {
   public class MainPresenter
@@ -9,55 +12,40 @@ namespace NumericalMethodsApp
       this.view = view;
     }
 
-    public void OpenMethod(string methodName)
+    public void OpenMethod(string methodName, Button button)
     {
-      string message = $"Открытие метода: {methodName}\n\n";
-
       switch (methodName)
       {
         case "Method1":
-          message = "Метод дихотомии уже открыт в отдельном окне.";
-          break;
-        case "Method5":
-          message = "Решение СЛАУ уже открыто в отдельном окне.";
+          DichotomyMethod dichotomyWindow = new DichotomyMethod();
+          view.OpenMethodWindow(methodName, dichotomyWindow, button);
           break;
         case "Method2":
-          message += "Метод золотого сечения будет реализован в будущем.";
+          GoldenRatioView goldenRatioWindow = new GoldenRatioView();
+          view.OpenMethodWindow(methodName, goldenRatioWindow, button);
           break;
-        case "Method3":
-          message += "Метод покоординатного спуска будет реализован в будущем.";
-          break;
-        case "Method4":
-          message += "Олимпиадные сортировки будут реализованы в будущем.";
-          break;
-        case "Method6":
-          message += "Метод Ньютона будет реализован в будущем.";
-          break;
-        case "Method7":
-          message += "Метод наименьших квадратов будет реализован в будущем.";
-          break;
-        case "Method8":
-          message += "Решение определенного интеграла будет реализовано в будущем.";
+        case "Method5":
+          LinearEquationsView slauWindow = new LinearEquationsView();
+          view.OpenMethodWindow(methodName, slauWindow, button);
           break;
         default:
-          message += "Эта функциональность будет реализована в будущем.";
+          string message = $"Метод {methodName} будет реализован в будущем.";
+          view.ShowMessage(message);
           break;
       }
-
-      view.ShowMessage(message);
     }
 
     public void ShowAbout()
     {
       string aboutText = "Данное приложение предназначено для реализации следующих численных методов:\n\n" +
-                         "- Метод дихотомии\n" +
-                         "- Решение СЛАУ (Гаусс, Жордан-Гаусс, Крамер)\n" +
-                         "- Метод Ньютона\n" +
-                         "- Метод золотого сечения\n" +
-                         "- Олимпиадные сортировки\n" +
-                         "- Метод покоординатного спуска\n" +
-                         "- Метод наименьших квадратов\n" +
-                         "- Решение определенного интеграла";
+                       "- Метод дихотомии\n" +
+                       "- Решение СЛАУ (Гаусс, Жордан-Гаусс, Крамер)\n" +
+                       "- Метод Ньютона\n" +
+                       "- Метод золотого сечения\n" +
+                       "- Олимпиадные сортировки\n" +
+                       "- Метод покоординатного спуска\n" +
+                       "- Метод наименьших квадратов\n" +
+                       "- Решение определенного интеграла";
 
       view.ShowMessage(aboutText);
     }
