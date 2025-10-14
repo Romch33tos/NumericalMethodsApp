deab815f926be9b311093beb0c918067eab74942
Roman Khadraliev <Romch33tos@yandex.ru>
2025-10-14 20:00:04 +0700

    Реализован интерфейс представления

---
+++ GoldenRatioMethod/IGoldenRatioView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumericalMethodsApp.Views
+{
+  public interface IGoldenRatioView
+  {
+    string FunctionExpression { get; set; }
+    string LowerBound { get; set; }
+    string UpperBound { get; set; }
+    string Epsilon { get; set; }
+    bool FindMinimum { get; set; }
+    bool FindMaximum { get; set; }
+    string ResultText { get; set; }
+
+    event EventHandler CalculateRequested;
+    event EventHandler ClearAllRequested;
+    event EventHandler HelpRequested;
+    event EventHandler ModeChanged;
+
+    void ShowError(string message);
+    void ShowInfo(string message);
+    void UpdatePlot(double lowerBound, double upperBound, double extremumX, double extremumY, bool isMinimum);
+    void ClearPlot();
+  }
+}
