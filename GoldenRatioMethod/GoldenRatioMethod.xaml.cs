985241ff554912ee5921a81fb460848704245c66
Roman Khadraliev <Romch33tos@yandex.ru>
2025-10-14 19:50:12 +0700

    Добавлены свойства для входных данных

--- GoldenRatioMethod/GoldenRatioMethod.xaml.cs
+++ GoldenRatioMethod/GoldenRatioMethod.xaml.cs
@@ -2,21 +2,16 @@ using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using OxyPlot;
 using NumericalMethodsApp.Presenters;
 
 namespace NumericalMethodsApp.Views
 {
-  public partial class GoldenRatioMethod : Window, IGoldenRatioView, INotifyPropertyChanged
+  public partial class GoldenRatioMethod : Window, INotifyPropertyChanged
   {
     private string _functionExpression;
     private string _lowerBound;
     private string _upperBound;
     private string _epsilon = "0.001";
-    private bool _findMinimum;
-    private bool _findMaximum;
-    private string _resultText;
 
     public GoldenRatioPresenter Presenter { get; private set; }
 
@@ -24,6 +19,46 @@ namespace NumericalMethodsApp.Views
     {
       InitializeComponent();
       Presenter = new GoldenRatioPresenter(this);
+    }
+
+    public string FunctionExpression
+    {
+      get => _functionExpression;
+      set
+      {
+        _functionExpression = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string LowerBound
+    {
+      get => _lowerBound;
+      set
+      {
+        _lowerBound = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string UpperBound
+    {
+      get => _upperBound;
+      set
+      {
+        _upperBound = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string Epsilon
+    {
+      get => _epsilon;
+      set
+      {
+        _epsilon = value;
+        OnPropertyChanged();
+      }
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
