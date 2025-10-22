5aef546de1f08c35751cd5d9438a3a4c046b66bf
Roman Khadraliev <Romch33tos@yandex.ru>
2025-10-22 16:23:18 +0700

    Добавлены основные свойства для View

---
+++ NewtonMethod/INewtonView.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace NumericalMethodsApp.Views
+{
+  public interface INewtonView
+  {
+    string FunctionExpression { get; set; }
+    string InitialPoint { get; set; }
+    string DisplayIntervalStart { get; set; }
+    string DisplayIntervalEnd { get; set; }
+    string Epsilon { get; set; }
+    bool FindMinimum { get; set; }
+    bool FindMaximum { get; set; }
+    string ResultText { get; set; }
+    bool CalculationInProgress { get; set; }
+
+    event EventHandler CalculateRequested;
+    event EventHandler ClearAllRequested;
+    event EventHandler HelpRequested;
+    event EventHandler ModeChanged;
+    event EventHandler NextStepRequested;
+
+    void ShowError(string message);
+    void ShowInfo(string message);
+  }
+}
