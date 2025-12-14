bd159b0d22ef4370eb93329935338736a0856982
Roman Khadraliev <Romch33tos@yandex.ru>
2025-12-14 21:55:36 +0700

    Реализован интерфейс представления метода

---
+++ CoordinateDescentMethod/ICoordinateDescentView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumericalMethodsApp
+{
+  public interface ICoordinateDescentView
+  {
+    string FunctionExpression { get; set; }
+    string XStart { get; set; }
+    string YStart { get; set; }
+    string Epsilon { get; set; }
+    string Result { get; set; }
+
+    event EventHandler CalculateClicked;
+    event EventHandler ClearAllClicked;
+    event EventHandler HelpClicked;
+
+    void ShowError(string message);
+    void ClearResults();
+    void UpdatePlot(OxyPlot.PlotModel plotModel); 
+  }
+}
